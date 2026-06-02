924f8aff913b12e237fe72814cfbd970908eb2df
nickdoa <nickdoa@users.noreply.github.com>
2026-06-02 10:05:20 -0500

    Prepare SoulGems 1.3.3

--- src/main/java/me/vexmc/enchantments/command/AuthorsCommand.java
+++ src/main/java/me/vexmc/enchantments/command/AuthorsCommand.java
@@ -12,32 +12,47 @@ import org.jetbrains.annotations.NotNull;
 
 public class AuthorsCommand implements CommandExecutor {
     @Override
-    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command,
+                             @NotNull String label, @NotNull String[] args) {
         if (!sender.hasPermission("soulgems.command.authors")) {
-            sender.sendMessage("§cYou do not have permission to use this command.");
+            sender.sendMessage(Component.text("You do not have permission to use this command.")
+                    .color(NamedTextColor.RED));
             return true;
         }
 
         Component emptyLine = Component.text("");
-        Component line1 = Component.text("This plugin was created with ")
+        Component title = Component.text("SoulGems Credits")
+                .color(NamedTextColor.GOLD)
+                .decorate(TextDecoration.BOLD);
+        Component originalHeader = Component.text("Original plugin created with ")
                 .color(NamedTextColor.GREEN)
-                .append(Component.text("♥")
+                .append(Component.text("\u2665")
                         .color(NamedTextColor.RED))
                 .append(Component.text(" by:")
                         .color(NamedTextColor.GREEN));
-        Component line2 = Component.text("owengregson")
+        Component originalAuthor = Component.text("owengregson")
                 .color(NamedTextColor.YELLOW)
-                .decorate(TextDecoration.UNDERLINED)
-                .hoverEvent(HoverEvent.showText(Component.text("Click to view my GitHub profile!")))
+                .hoverEvent(HoverEvent.showText(Component.text("Click to view the GitHub profile.")))
                 .clickEvent(ClickEvent.openUrl("https://github.com/owengregson"))
                 .append(Component.text(" on GitHub.")
                         .color(NamedTextColor.GREEN)
-                        .decoration(TextDecoration.UNDERLINED, TextDecoration.State.FALSE)
-                );
+                        .decoration(TextDecoration.UNDERLINED, TextDecoration.State.FALSE));
+        Component modifiedBy = Component.text(" Modified by ")
+                .color(NamedTextColor.GREEN)
+                .decoration(TextDecoration.UNDERLINED, TextDecoration.State.FALSE)
+                .append(Component.text("nickdoa")
+                        .color(NamedTextColor.YELLOW)
+                        .decorate(TextDecoration.UNDERLINED)
+                        .hoverEvent(HoverEvent.showText(Component.text("Click to view the GitHub profile.")))
+                        .clickEvent(ClickEvent.openUrl("https://github.com/nickdoa")))
+                .append(Component.text(".")
+                        .color(NamedTextColor.GREEN)
+                        .decoration(TextDecoration.UNDERLINED, TextDecoration.State.FALSE));
 
         sender.sendMessage(emptyLine);
-        sender.sendMessage(line1);
-        sender.sendMessage(line2);
+        sender.sendMessage(title);
+        sender.sendMessage(originalHeader);
+        sender.sendMessage(originalAuthor.append(modifiedBy));
         sender.sendMessage(emptyLine);
         return true;
     }
